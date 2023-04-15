c94e3f83d814ef6b3b33b19f6798ec6192e915dd
Upenha <contato.upenha@gmail.com>
2023-04-15 18:05:32 -0300

    testando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ const exo = Exo_2({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={`${exo.className} scroll-smooth flex w-full h-full overflow-x-hidden bg-background bg-center text-white justify-center flex-col items-center`}>
+    <main className={`${exo.className} scroll-smooth flex w-full h-full overflow-x-hidden bg-zinc-900 bg-background bg-center text-white justify-center flex-col items-center`}>
       <section className="flex flex-col items-center justify-center w-screen h-screen text-white bg-center bg-background">
         <div className="flex flex-row items-center justify-around w-screen h-screen">
-          <div className="flex flex-row gap-1 w-2/3 items-center justify-around max-sm:gap-0">
+          <div className="flex flex-row items-center justify-around w-2/3 gap-1 max-sm:gap-0">
             <div>
               <p className="text-xl font-semibold text-white max-sm:text-base">howdy!</p>
               <h1 className="text-6xl font-extrabold text-purple-600 max-sm:text-3xl">
@@ -20,7 +20,7 @@ export default function Home() {
               <h1 className="text-5xl font-bold text-purple-600 max-sm:text-2xl">
                 kouzoukian
               </h1>
-              <p className="mt-3 mb-6 max-sm:mt-2 max-sm:mb-4 text-lg text-zinc-400 max-sm:text-sm">designer and software engineer</p>
+              <p className="mt-3 mb-6 text-lg max-sm:mt-2 max-sm:mb-4 text-zinc-400 max-sm:text-sm">designer and software engineer</p>
             </div>
             <Image src='/photo.jpg' alt='' width={250} height={250} className="p-2 rounded-full ring-purple-700 ring-4 max-sm:w-1/3 max-sm:h-1/3 max-sm:ring-2 max-sm:p-1"/>
           </div>
@@ -34,7 +34,7 @@ export default function Home() {
         <h1 className="text-6xl font-extrabold text-purple-600 max-sm:text-4xl">
           About me
         </h1>
-        <div className="flex flex-col w-1/3 pt-4 max-sm:w-2/3 gap-4">
+        <div className="flex flex-col w-1/3 gap-4 pt-4 max-sm:w-2/3">
           <p>
             Hello! I&apos;m Lucas &quot;Upenha&quot; Kouzoukian, a {dayjs(new Date()).diff('2008-04-21', 'y')}yo autodidact developer and designer that love learning new things. I&apos;m currently using Next.js, React.js, Typescript, Node.js, Mongodb and other technologies! I have so many private projects including <a href="https://github.com/zorinbot/" className="font-bold underline decoration-white">@zorinbot</a> and non-remunerated works in <a href="https://squarecloud.app/" className="font-bold underline decoration-white">@squarecloud</a>  
           </p>
