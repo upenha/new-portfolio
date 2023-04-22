2d3d0905dff67231436e441682a69462cbba324a
Upenha <contato.upenha@gmail.com>
2023-04-22 18:03:25 -0300

    omg favicon sucks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,23 @@ import './globals.css'
 
 export const metadata = {
   title: 'Upenha',
-  description: 'Made with ❤!',
+  description: "Hello i'm Lucas AKA Upenha, a young developer and designer that loves learning new things.",
+  openGraph: {
+    title: 'Upenha',
+    description: "Hello i'm Lucas AKA Upenha, a young developer and designer that loves learning new things.",
+    url: 'https://upenha.tech',
+    siteName: 'Upenha',
+    locale: 'en-US',
+    images: [
+      {
+        url: 'https://github.com/upenha.png',
+      }
+    ],
+    type: 'website'
+  },
+  themeColor: 'black',
 }
+
 
 export default function RootLayout({
   children,
